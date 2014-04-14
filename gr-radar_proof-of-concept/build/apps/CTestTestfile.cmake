f78b03fcfd482447bfc7292b06155b8b534b8b9b
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2014-04-14 17:56:44 +0200

    delete build

--- gr-radar_proof-of-concept/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stefan/git/radsens/gr-radar/apps
-# Build directory: /home/stefan/git/radsens/gr-radar/build/apps
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-radar_proof-of-concept/build/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stefan/git/radsens/gr-radar/docs
-# Build directory: /home/stefan/git/radsens/gr-radar/build/docs
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
